f9c8d15f6d49b3174b043bfbe7ec4db08f1fce6f
Fernando Rivera <frivera@timesolution.com.ar>
2019-03-08 13:53:23 -0300

    no message

--- Gestion Web/Formularios/Compras/Entregas/EntregasF.aspx
+++ Gestion Web/Formularios/Compras/Entregas/EntregasF.aspx
@@ -1,3 +1,128 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EntregasF.aspx.cs" Inherits="Gestion_Web.Formularios.Compras.Entregas.EntregasF" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="col-md-12 col-xs-12">
+        <div class="widget stacked">
+            <div class="stat">
+                <h5><i class="icon-map-marker"></i>Compras > Entregas</h5>
+            </div>
+            <div class="widget-header">
+                <i class="icon-wrench"></i>
+                <h3>Herramientas</h3>
+            </div>
+            <div class="widget-content">
+                <table style="width: 100%">
+                    <tr>
+                        <td style="width: 20%">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" id="btnAccion" runat="server">Accion<span class="caret"></span></button>
+                                <ul class="dropdown-menu">
+                                </ul>
+                            </div>
+                        </td>
+                        <td style="width: 65%">
+                            <h5>
+                                <asp:Label runat="server" ID="lblParametros" Text="" ForeColor="#cccccc"></asp:Label>
+                            </h5>
+                        </td>
+                        <td style="width: 5%"></td>
+                        <td style="width: 5%">
+                            <div class="shortcuts" style="height: 100%">
+                                <a class="btn btn-primary" data-toggle="modal" href="#modalBusqueda" style="width: 100%">
+                                    <i class="shortcut-icon icon-filter"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12 col-xs-12">
+        <div class="widget widget-table">
+            <div class="widget-header">
+                <i class="icon-th-list" style="width: 2%"></i>
+                <h3 style="width: 75%">Compras
+                </h3>
+                <h3>
+                    <asp:Label ID="lblSaldo" runat="server" Style="text-align: right" Text="" ForeColor="#666666" Font-Bold="true"></asp:Label>
+                </h3>
+            </div>
+            <div class="widget-content">
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <a class="btn btn-info" style="display: none" data-toggle="modal" id="abreDialog" href="#modalFacturaDetalle">Agregar Tipo Cliente</a>
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th style="width: 10%">Fecha</th>
+                                    <th style="width: 10%">Fecha Entrega</th>
+                                    <th style="width: 15%">Numero</th>
+                                    <th style="width: 15%">Proveedor</th>
+                                    <th style="width: 20%">Sucursal</th>
+                                    <th style="width: 15%">Estado</th>
+                                    <th style="width: 15%"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <asp:PlaceHolder ID="phEntregas" runat="server"></asp:PlaceHolder>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="modalBusqueda" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title">Busqueda</h4>
+                </div>
+                <div class="modal-body">
+                    <div role="form" class="form-horizontal col-md-12">
+                        <fieldset>
+                            <div class="form-group">
+                                <label class="col-md-3">Fecha Orden Compra</label>
+                                <div class="col-md-3">
+                                    <asp:TextBox ID="txtFechaDesde" placeholder="Desde" runat="server" class="form-control"></asp:TextBox>
+                                </div>
+                                <div class="col-md-3">
+                                    <asp:TextBox ID="txtFechaHasta" placeholder="Hasta" runat="server" class="form-control"></asp:TextBox>
+                                </div>
+                                <div class="col-md-1">
+                                    <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="<h3>*</h3>" ControlToValidate="txtFechaDesde" ValidationGroup="BusquedaGroup" SetFocusOnError="true" ForeColor="Red" Font-Bold="true"></asp:RequiredFieldValidator>
+                                </div>
+                                <div class="col-md-1">
+                                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="<h3>*</h3>" ControlToValidate="txtFechaHasta" ValidationGroup="BusquedaGroup" SetFocusOnError="true" ForeColor="Red" Font-Bold="true"></asp:RequiredFieldValidator>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="name" class="col-md-3">Cod. Proveedor</label>
+                                <div class="col-md-4">
+                                    <asp:TextBox ID="txtCodProveedor" class="form-control" runat="server"></asp:TextBox>
+                                </div>
+                                <div class="col-md-5">
+                                    <asp:LinkButton ID="btnBuscarCodigoProveedor" runat="server" Text="<span class='shortcut-icon icon-search'></span>" class="btn btn-info" OnClick="btnBuscarCodigoProveedor_Click" />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="name" class="col-md-3">Proveedor</label>
+                                <div class="col-md-6">
+                                    <asp:DropDownList ID="ListProveedor" class="form-control" runat="server" AutoPostBack="True"></asp:DropDownList>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <asp:LinkButton ID="lbtnBuscar" runat="server" Text="<span class='shortcut-icon icon-ok'></span>" class="btn btn-success" ValidationGroup="BusquedaGroup" OnClick="lbtnBuscar_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- Gestion Web/Formularios/Compras/Entregas/EntregasF.aspx.cs
+++ Gestion Web/Formularios/Compras/Entregas/EntregasF.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Disipar.Models;
+using Gestion_Api.Modelo;
+using Gestor_Solution.Controladores;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,204 @@ namespace Gestion_Web.Formularios.Compras.Entregas
 {
     public partial class EntregasF : System.Web.UI.Page
     {
+        Mensajes m = new Mensajes();
+        private int proveedor;
+        private string fechaD;
+        private string fechaH;
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            VerificarLogin();
+
+            if (fechaD == null && fechaH == null)
+            {
+                fechaD = DateTime.Now.ToString("dd/MM/yyyy");
+                fechaH = DateTime.Now.ToString("dd/MM/yyyy");
+                
+                txtFechaDesde.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                txtFechaHasta.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            }
+
+            txtFechaDesde.Text = fechaD;
+            txtFechaHasta.Text = fechaH;
+            ListProveedor.SelectedValue = proveedor.ToString();
+        }
+
+        private void VerificarLogin()
+        {
+            try
+            {
+                if (Session["User"] == null)
+                {
+                    Response.Redirect("../../../Account/Login.aspx");
+                }
+                else
+                {
+                    if (this.verificarAcceso() != 1)
+                    //if (this.contUser.validarAcceso((int)Session["Login_IdUser"], "Herramientas.Presupuesto") != 1)
+                    {
+                        Response.Redirect("/Default.aspx?m=1", false);
+                    }
+                }
+            }
+            catch
+            {
+                Response.Redirect("../../../Account/Login.aspx");
+            }
+        }
+
+        private int verificarAcceso()
+        {
+            try
+            {
+                string permisos = Session["Login_Permisos"] as string;
+                //string[] listPermisos = permisos.Split(';');
+                //foreach (string s in listPermisos)
+                //{
+                //    if (!String.IsNullOrEmpty(s))
+                //    {
+
+                //    }
+                //}
+
+
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public void cargarProveedores()
+        {
+            try
+            {
+                controladorCliente contCliente = new controladorCliente();
+
+                DataTable dt = contCliente.obtenerProveedoresReducDT();
+
+                DataRow dr = dt.NewRow();
+                dr["alias"] = "Seleccione...";
+                dr["id"] = -1;
+                dt.Rows.InsertAt(dr, 0);
+
+                this.ListProveedor.DataSource = dt;
+                this.ListProveedor.DataValueField = "id";
+                this.ListProveedor.DataTextField = "alias";
+
+                this.ListProveedor.DataBind();
+            }
+            catch (Exception ex)
+            {
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", m.mensajeBoxError("Error cargando proveedores a la lista. " + ex.Message));
+            }
+        }
+
+        protected void lbtnBuscar_Click(object sender, EventArgs e)
         {
 
         }
+
+        protected void btnBuscarCodigoProveedor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controladorCliente contrCliente = new controladorCliente();
+                String buscar = this.txtCodProveedor.Text.Replace(' ', '%');
+                DataTable dtClientes = contrCliente.obtenerProveedorNombreDT(buscar);
+                this.phEntregas.Controls.Clear();
+
+                this.ListProveedor.DataSource = dtClientes;
+                this.ListProveedor.DataValueField = "id";
+                this.ListProveedor.DataTextField = "alias";
+                this.ListProveedor.DataBind();
+
+            }
+            catch (Exception Ex)
+            {
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", m.mensajeBoxError("Error cargando proveedores a la lista. Excepción: " + Ex.Message));
+            }
+        }
+
+        //public void CargarItemsFacturaEnPH()
+        //{
+        //    try
+        //    {
+        //        var ordenCompra = contComprasEnt.obtenerOrden(this.ordenCompra);
+
+        //        foreach (var item in ordenCompra.OrdenesCompra_Items)
+        //        {
+        //            if (item.CantidadYaRecibida < item.Cantidad)
+        //            {
+        //                var diferenciasOrdenCompra = ordenCompra.RemitoCompraOrdenCompra_Diferencias.Where(x => x.OrdenCompra == item.IdOrden && x.Articulo.ToString() == item.Codigo).ToList();
+        //                cargarEnPh(item, diferenciasOrdenCompra);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.EscribirSQL(1, "Error", "Error al cargar los items de la factura en el PH " + ex.Message);
+        //    }
+        //}
+
+        //private void cargarEnPh(OrdenesCompra_Items ocItem, List<RemitoCompraOrdenCompra_Diferencias> diferencias = null)
+        //{
+        //    try
+        //    {
+        //        //fila
+        //        TableRow tr = new TableRow();
+        //        tr.ID = ocItem.Codigo.ToString();
+
+        //        //Celdas
+        //        TableCell celCodigo = new TableCell();
+        //        celCodigo.Text = contArticulos.obtenerArticuloByID(Convert.ToInt32(ocItem.Codigo)).codigo;
+        //        celCodigo.HorizontalAlign = HorizontalAlign.Left;
+        //        celCodigo.VerticalAlign = VerticalAlign.Middle;
+        //        tr.Cells.Add(celCodigo);
+
+        //        TableCell celDescripcion = new TableCell();
+        //        celDescripcion.Text = ocItem.Descripcion;
+        //        celDescripcion.HorizontalAlign = HorizontalAlign.Left;
+        //        celDescripcion.VerticalAlign = VerticalAlign.Middle;
+        //        tr.Cells.Add(celDescripcion);
+
+        //        TableCell celCantidad = new TableCell();
+        //        decimal cantidad = Convert.ToDecimal(ocItem.Cantidad);
+        //        celCantidad.Text = cantidad.ToString();
+        //        celCantidad.HorizontalAlign = HorizontalAlign.Left;
+        //        celCantidad.VerticalAlign = VerticalAlign.Middle;
+        //        tr.Cells.Add(celCantidad);
+
+        //        TableCell celCantidadYaRecibida = new TableCell();
+        //        decimal cantidadYaRecibidas = ObtenerCantidadesYaRecibidas(diferencias);
+        //        celCantidadYaRecibida.Text = "0";
+
+        //        if (diferencias != null && diferencias.Count > 0)
+        //            celCantidadYaRecibida.Text = cantidadYaRecibidas.ToString();
+
+        //        celCantidadYaRecibida.HorizontalAlign = HorizontalAlign.Left;
+        //        celCantidadYaRecibida.VerticalAlign = VerticalAlign.Middle;
+        //        tr.Cells.Add(celCantidadYaRecibida);
+
+        //        TableCell celAccion = new TableCell();
+
+        //        TextBox celCantidadRecibida = new TextBox();
+        //        celCantidadRecibida.TextMode = TextBoxMode.Number;
+        //        celCantidadRecibida.Attributes.Add("onkeypress", "javascript:return validarNro(event)");
+        //        celCantidadRecibida.Text = (cantidad - cantidadYaRecibidas).ToString();
+
+        //        celAccion.Controls.Add(celCantidadRecibida);
+
+        //        tr.Cells.Add(celAccion);
+
+        //        phProductos.Controls.Add(tr);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", m.mensajeBoxError("Error en fun cargarEnPh EntregasMercaderiaF. Ex: " + ex.Message));
+        //    }
+        //}
     }
 }
