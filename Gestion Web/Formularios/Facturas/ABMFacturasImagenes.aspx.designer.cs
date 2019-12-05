0dcb47e7db2c518e7cadfeaf251574d44836ecd6
ramiroprk <ramiroprk@gmail.com>
2019-12-05 09:54:39 -0300

    boton de cliente scrap en facturas imagenes

--- Gestion Web/Formularios/Facturas/ABMFacturasImagenes.aspx.designer.cs
+++ Gestion Web/Formularios/Facturas/ABMFacturasImagenes.aspx.designer.cs
@@ -2083,6 +2083,15 @@ namespace Gestion_Web.Formularios.Facturas {
         protected global::System.Web.UI.WebControls.LinkButton lbND;
         
         /// <summary>
+        /// btnSetearClienteScrap1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnSetearClienteScrap1;
+        
+        /// <summary>
         /// lbFormaDePago control.
         /// </summary>
         /// <remarks>
