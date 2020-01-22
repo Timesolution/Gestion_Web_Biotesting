85bc71c016c7bb33c0b10a461ad11e06e2035c33
Fernando Rivera <frivera@timesolution.com.ar>
2020-01-22 10:18:45 -0300

    no message

--- Gestion Web/Formularios/Articulos/Articulos.aspx.cs
+++ Gestion Web/Formularios/Articulos/Articulos.aspx.cs
@@ -1159,6 +1159,10 @@ namespace Gestion_Web.Formularios.Articulos
                     {
                         ceros = "-1";
                     }
+                    if (CheckIncluirPendientes.Checked)
+                    {
+                        Response.Redirect("ReporteAF.aspx?e=1&accion=7&s=" + this.listSucursal.SelectedValue + "&g=" + this.grupo + "&sg=" + this.subgrupo + "&p=" + this.proveedor + "&d=" + this.dias + "&m=" + this.marca);
+                    }
                     if (this.CheckBoxUnicoSucursal.Checked)
                     {
                         //ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "window.open('ReporteAF.aspx?accion=6&s=" + this.listSucursal.SelectedValue + "&g=" + this.grupo + "&sg=" + this.subgrupo + "&p=" + this.proveedor + "&d=" + this.dias + "&c=" + ceros + "', 'fullscreen', 'top=0,left=0,width='+(screen.availWidth)+',height ='+(screen.availHeight)+',fullscreen=yes,toolbar=0 ,location=0,directories=0,status=0,menubar=0,resiz able=0,scrolling=0,scrollbars=0');", true);
@@ -1180,6 +1184,10 @@ namespace Gestion_Web.Formularios.Articulos
                     if (this.CheckBoxStockFaltante.Checked)
                     {
                         ceros = "-1";
+                    }
+                    if (CheckIncluirPendientes.Checked)
+                    {
+                        Response.Redirect("ReporteAF.aspx?e=1&accion=7&s=" + this.listSucursal.SelectedValue + "&g=" + this.grupo + "&sg=" + this.subgrupo + "&p=" + this.proveedor + "&d=" + this.dias + "&m=" + this.marca);
                     }
                     if (this.CheckBoxUnicoSucursal.Checked)
                     {
